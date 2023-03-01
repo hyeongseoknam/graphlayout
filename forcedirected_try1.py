f398343e1bc0049d1a7f85f3bf3f3c8135391fd2
남형석 <hsnam@whatap.io>
2023-03-01 15:24:20 +0900

    chatgpt graph force directed

---
+++ forcedirected_try1.py
@@ -0,0 +1,38 @@
+import math
+import random
+
+def force_directed_layout(graph, iterations=50, k=0.1, c=0.01, p=0.1):
+    """
+    Computes the coordinates of nodes in a graph using the force-directed algorithm.
+
+    Parameters:
+        - graph (dict): A dictionary representing the graph. The keys are the node IDs and the values are lists of
+          the IDs of the nodes that the key node is connected to.
+        - iterations (int): The number of iterations to run the algorithm for.
+        - k (float): The spring constant.
+        - c (float): The damping factor.
+        - p (float): The repulsion exponent.
+
+    Returns:
+        - A dictionary representing the layout of the graph. The keys are the node IDs and the values are tuples of
+          the x and y coordinates of the node.
+    """
+
+    # Initialize the layout randomly.
+    layout = {node_id: (random.random(), random.random()) for node_id in graph}
+
+    # Run the algorithm for the specified number of iterations.
+    for _ in range(iterations):
+        # Compute the repulsive forces between nodes.
+        repulsive_forces = {node_id: [0, 0] for node_id in graph}
+        for node_id1 in graph:
+            for node_id2 in graph:
+                if node_id1 != node_id2:
+                    x1, y1 = layout[node_id1]
+                    x2, y2 = layout[node_id2]
+                    dx, dy = x1 - x2, y1 - y2
+                    d = max(math.sqrt(dx ** 2 + dy ** 2), 0.0001)
+                    force = k ** p / d ** p
+                    repulsive_forces[node_id1][0] += force * dx / d
+                    repulsive_forces[node_id1][1] += force * dy / d
+
